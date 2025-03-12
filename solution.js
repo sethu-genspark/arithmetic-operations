1bb64802ee71878e125ef109f2bc259fca203abf
Sethu Ravichandran <sethu@Sethus-MacBook-Air.local>
2025-03-12 10:30:14 +0530

    ADD - Included the functionality to find the sum of two numbers.

--- solution.js
+++ solution.js
@@ -1,1 +1,8 @@
-console.log(`Hello world !`)
+let firstInput = 3
+let secondInput = 5
+
+function calculateSum(firstNumber, secondNumber){
+    return firstNumber + secondNumber
+}
+
+console.log(calculateSum(firstInput, secondInput))
