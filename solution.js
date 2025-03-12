e4709c606b63336e2ff19f11999d4e54d42181ed
Sethu Ravichandran <sethu@Sethus-MacBook-Air.local>
2025-03-12 10:31:25 +0530

    ADD - Included the functionality to find the difference between two numbers.

--- solution.js
+++ solution.js
@@ -5,4 +5,9 @@ function calculateSum(firstNumber, secondNumber){
     return firstNumber + secondNumber
 }
 
+function calculateDifference(firstNumber, secondNumber){
+    return firstNumber - secondNumber
+}
+
 console.log(calculateSum(firstInput, secondInput))
+console.log(calculateDifference(firstInput, secondInput))
